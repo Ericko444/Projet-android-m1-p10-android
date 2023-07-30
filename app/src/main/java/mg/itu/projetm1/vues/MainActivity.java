aeb05f854af440652aa8d36335fa5eea0871ab03
Ericko Manarivo <manarivoericko4@gmail.com>
2023-07-30 11:56:45 +0300

    Created bottom navigation

--- app/src/main/java/mg/itu/projetm1/vues/MainActivity.java
+++ app/src/main/java/mg/itu/projetm1/vues/MainActivity.java
@@ -1,16 +1,50 @@
 package mg.itu.projetm1.vues;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import mg.itu.projetm1.R;
+import mg.itu.projetm1.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.explore:
+                    replaceFragment(new ExploreFragment());
+                    break;
+                case R.id.saved:
+                    replaceFragment(new SavedFragment());
+                    break;
+                case R.id.account:
+                    replaceFragment(new AccountFragment());
+                    break;
+            }
+
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
