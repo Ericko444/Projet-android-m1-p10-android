d6e30df0a196a0b671124b79e0840a6453f67f9a
HenikaLucas <60760230+HenikaLucas@users.noreply.github.com>
2023-08-06 18:27:19 +0300

    logical setting

--- app/src/main/java/mg/itu/projetm1/vues/SettingFragment.java
+++ app/src/main/java/mg/itu/projetm1/vues/SettingFragment.java
@@ -1,21 +1,33 @@
 package mg.itu.projetm1.vues;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 
 import mg.itu.projetm1.R;
 import mg.itu.projetm1.session.SessionManager;
 
 public class SettingFragment extends Fragment {
+
+    private static final String PREF_NAME = "my_preferences";
+    private static final String KEY_DARK_MODE = "dark_mode";
+    private static final String KEY_NOTIFICATIONS = "notifications";
+    private SharedPreferences sharedPreferences;
+    private Switch switchDarkMode;
+    private Switch switchNotifications;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -47,6 +59,44 @@ public class SettingFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         TextView appVersionTextView = view.findViewById(R.id.app_version_text);
         appVersionTextView.setText(getAppVersion());
+        getPrefs(view);
+    }
+
+    private void getPrefs(View view) {
+        sharedPreferences = requireContext().getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+
+        switchDarkMode = view.findViewById(R.id.switch_dark_mode);
+        switchNotifications = view.findViewById(R.id.switch_notifications);
+
+        boolean isDarkModeEnabled = sharedPreferences.getBoolean(KEY_DARK_MODE, false);
+        boolean areNotificationsEnabled = sharedPreferences.getBoolean(KEY_NOTIFICATIONS, true);
+
+        switchDarkMode.setChecked(isDarkModeEnabled);
+        switchNotifications.setChecked(areNotificationsEnabled);
+
+        switchDarkMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean(KEY_DARK_MODE, isChecked);
+                editor.apply();
+
+                if (isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                }
+            }
+        });
+
+        switchNotifications.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putBoolean(KEY_NOTIFICATIONS, isChecked);
+                editor.apply();
+            }
+        });
     }
 
     private String getAppVersion() {
