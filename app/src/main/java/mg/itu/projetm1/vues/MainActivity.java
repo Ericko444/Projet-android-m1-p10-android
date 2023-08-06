8bac451d198a484126c4bdce3608c599490aa998
HenikaLucas <60760230+HenikaLucas@users.noreply.github.com>
2023-08-06 23:12:23 +0300

    remove unused files

--- app/src/main/java/mg/itu/projetm1/vues/MainActivity.java
+++ app/src/main/java/mg/itu/projetm1/vues/MainActivity.java
@@ -1,5 +1,17 @@
 package mg.itu.projetm1.vues;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.ColorStateList;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.TextAppearanceSpan;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
@@ -14,27 +26,8 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.res.ColorStateList;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.text.SpannableString;
-import android.text.style.TextAppearanceSpan;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
-
-import java.util.HashMap;
 
 import mg.itu.projetm1.R;
 import mg.itu.projetm1.databinding.ActivityMainBinding;
@@ -83,9 +76,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     private void checkPreferences() {
-        SharedPreferences sharedPreferences = getSharedPreferences(SettingFragment.PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences sharedPreferences = getSharedPreferences(SettingActivity.PREF_NAME, Context.MODE_PRIVATE);
         if(sharedPreferences != null){
-            boolean isDarkModeEnabled = sharedPreferences.getBoolean(SettingFragment.KEY_DARK_MODE, false);
+            boolean isDarkModeEnabled = sharedPreferences.getBoolean(SettingActivity.KEY_DARK_MODE, false);
             if (isDarkModeEnabled) {
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
             } else {
